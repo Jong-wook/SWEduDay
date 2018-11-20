d3e4cbcea5583d8f0c2b5055ce2bd6b66d03c598
Jongwook Kim <jongwook1208@gmail.com>
2018-11-20 07:20:29 +0000

    seconde2

--- Node_SW_dir/app.js
+++ Node_SW_dir/app.js
@@ -39,15 +39,31 @@ app.post('/process/application', function(req, res){
 		paramCoding = req.body.coding_exp,
 		paramMotivation = req.body.motivation;
 	var params = [paramName,paramPhone,paramSchool,paramGrade,paramSWEducation,paramCoding,paramMotivation];
-	var sql = 'INSERT INTO applicant (name,phone,school,grade,sw_education_exp, coding_exp, motivation) values (?,?,?,?,?,?,?)';
+	var sql ="IF (NOT EXISTS ( SELECT * FROM applicant WHERE name = '"+ paramName +"' AND phone = '"+ paramPhone +"' )" 
+		+"INSERT INTO applicant (name,phone,school,grade,sw_education_exp, coding_exp, motivation)"
+                + " values ('"+paramName+"','"+paramPhone+"','"+paramSchool+"','"+paramGrade+"','"
+                +paramSWEducation+"','"+paramCoding+"','"+paramMotivation+"') ";
+	
+	var sql2 = "INSERT INTO applicant (name,phone,school,grade,sw_education_exp, coding_exp, motivation)"
+		+ " values ('"+paramName+"','"+paramPhone+"','"+paramSchool+"','"+paramGrade+"','"
+		+paramSWEducation+"','"+paramCoding+"','"+paramMotivation+"') ";
+	//+" values (?,?,?,?,?,?,?);";
+	
+	sql= sql+"AS TEMP WHERE NOT EXISTS ( SELECT * FROM applicant WHERE name = '"+ paramName +"' AND phone = '"+ paramPhone +"' );";
 	//var params = ['Jongwook', '01063302476','서울해광동초등학교' ,4,0,1,'열심히할게요' ];
-        connection.query(sql,params, function(err,rows, fields ){
-                if(err) { console.log(err) }
-                else { console.log(lows) }
+        connection.query(sql, function(err,rows, fields ){
+	//connection.query(sql,params, function(err,rows, fields ){
+                if(err) { 
+			console.log(err);
+			}
+                else {
+			console.log(lows);
+			}
         });
         connection.end();
 
-    res.send('Success Application !! '+params);
+        res.send('교육신청이 완료되었습니다. '+params);
+
 });
 
 app.post('/process/search', function(req, res){
@@ -62,8 +78,8 @@ app.post('/process/search', function(req, res){
         var paramName = req.body.name,
                 paramPhone = req.body.phone;
         //var params = [paramName,paramPhone];
-        var sql = 'SELECT name FROM applicant WHERE name = '+ paramName + ' AND phone = '+paramPhone+';';
-        //var params = ['Jddddaongwook', '01063302476','서울해광동초등학교' ,4,0,1,'열심히할게요' ];
+        var sql = "SELECT name FROM applicant WHERE name = '"+  paramName + "' AND phone = '"+ paramPhone+"';";
+        
         connection.query(sql, function(err,rows, fields ){
                 if(err) { console.log(err) }
                 else 	{ 
@@ -80,32 +96,7 @@ app.post('/process/search', function(req, res){
 });
 
 var router = express.Router();
-/*
-router.route('/process/formResponse').post(function(req, res) {
-	console.log('/process/formResponse 호출됨.');
 
-	var paramId = req.body.id || req.query.id;
-	var paramPassword = req.body.password || req.query.password;
-	
-	if (req.session.user) {
-		
-		res.redirect('/public/product.html');
-	} else {
-		// 세션 저장
-		req.session.user = {
-			id: paramId,
-			name: '이름',
-			authorized: true
-		};
-		
-		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.write('<h1>로그인 성공</h1>');
-		res.write('<div><p>Param id : ' + paramId + '</p></div>');
-		res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-		res.write("<br><br><a href='/process/product'>상품 페이지로 이동하기</a>");
-		res.end();
-	}
-}); */
 app.listen(8000, function(){
     console.log('Conneted 8000 port!');
 });
